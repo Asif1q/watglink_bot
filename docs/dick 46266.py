80d4a513e65bcbbfe106f316612e2a71e898b1e5
Asif1q <54772037+Asif1q@users.noreply.github.com>
2020-12-10 13:57:11 +0530

    Add files via upload

---
+++ docs/dick 46266.py
@@ -0,0 +1,23 @@
+"""Emoji
+Available Commands:
+.dick
+`@rooternobody`"""
+
+from telethon import events
+
+import asyncio
+
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.dick")
+async def dick(event):
+    if event.fwd_from:
+        return
+    animation_interval = 2.5
+    animation_ttl = range(0, 1)
+    animation_chars = ['...............▄▄ ▄▄\n......▄▌▒▒▀▒▒▐▄\n.... ▐▒▒▒▒▒▒▒▒▒▌\n... ▐▒▒▒▒▒▒▒▒▒▒▒▌\n....▐▒▒▒▒▒▒▒▒▒▒▒▌\n....▐▀▄▄▄▄▄▄▄▄▄▀▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n....▐░░░░░░░░░░░▌\n...▄█▓░░░░░░░░░▓█▄\n..▄▀░░░░░░░░░░░░░ ▀▄\n.▐░░░░░░░▀▄▒▄▀░░░░░░▌\n▐░░░░░░░▒▒▐▒▒░░░░░░░▌\n▐▒░░░░░▒▒▒▐▒▒▒░░░░░▒▌\n.▀▄▒▒▒▒▒▄▀▒▀▄▒▒▒▒▒▄▀\n.. ▀▀▀▀▀ ▀▀▀▀▀']
+
+    for i in animation_ttl:
+
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i %25 ])
